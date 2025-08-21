38798410dd66ffce8d69fdfa39b1f38302969a87
Nahid Hasan <nahidhasan9613@gmail.com>
2025-08-21 11:18:15 +0600

    load secret key

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    GOOGLE_API_KEY: str
+    GOOGLE_DRIVE_CREDENTIALS_FILE: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
